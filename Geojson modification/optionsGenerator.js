c0beeb32a23a2fd7e67a2008fb9a97851b62fbb8
Rinze <rinze.bruining@tno.nl>
2016-06-24 15:22:26 +0200

    Added script to generate a list of options (to be used in a propertyType) from a text-file with one title per line.

---
+++ Geojson modification/optionsGenerator.js
@@ -0,0 +1,78 @@
+// Creates options list from a text file having one option id per line. Result in the form of
+//"options": {
+//              "0": "Eengezinswoning",
+//              "1": "Appartement"
+//           }
+//USAGE:
+// input parameter 1: options to convert (text file with one option id per line)
+// input parameter 2: output file
+// input parameter 3: (optional) use title as id (default false) Ex. "Eengezinswoning": "Eengezinswoning", "Appartement": "Appartement"
+var fs = require('fs');
+var path = require('path');
+var filePath;
+var outFile;
+
+var useTitleId = false;
+
+var printMsg;
+if (process.argv.length < 4) {
+   console.log('Too few input parameters. Exiting...');
+   process.exit();
+} 
+process.argv.forEach(function (val, index, array) {
+    switch(index) {
+        case 0:
+        case 1:
+            break;
+        case 2:
+            filePath = path.join(__dirname, val);
+            printMsg = 'Input file: ' + filePath;
+            break;
+        case 3: 
+            outFile = path.join(__dirname, val);
+            printMsg = 'Output file: ' + outFile;
+            break;
+        case 4: 
+            useTitleId = true;
+            printMsg = 'Use Title as id';
+            break;
+        default:
+            printMgs = 'Additional parameter ignored';
+            break;            
+    }
+    if (printMsg) console.log(printMsg);
+});
+
+
+fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (!err){
+        console.log('Received data successfully');
+        var allIds = data;
+        this.ConvertData(allIds);
+    }else{
+        console.log(err);
+    }
+});
+
+//"options": {
+ //               "0": "Eengezinswoning",
+  //              "1": "Appartement"
+   //         },
+
+ConvertData = function (data) {
+    var allIds = data.split('\n');
+    var count = 0;
+    var result = {};
+    result['options'] = {};
+    allIds.forEach(function(opt) {
+        if (useTitleId) {
+            result['options'][opt] = opt;
+        } else {
+            result['options'][count.toFixed(0)] = opt;
+            count+=1;
+        }
+    });
+    fs.writeFileSync(outFile, JSON.stringify(result, null, 2));
+    console.log('Generated options! ' + count);
+};
+						
